7a672b1b52d379aac367347f12493460ff8be7fe
Colt <98503459+coltschultz@users.noreply.github.com>
2022-05-15 22:37:07 -0500

    edit/delete comments

--- public/javascript/comment.js
+++ public/javascript/comment.js
@@ -26,4 +26,25 @@ async function handleComment(event) {
   }
 }
 
+async function deleteCommentFormHandler(event) {
+  event.preventDefault();
+
+  const id = window.location.toString().split("/")[
+    window.location.toString().split("/").length - 1
+  ];
+  const response = await fetch(/api/comments/${id}, {
+    method: "DELETE",
+  });
+
+  if (response.ok) {
+    document.location.replace("/dashboard/");
+  } else {
+    alert(response.statusText);
+  }
+}
+
+document
+  .querySelector("#delete-comment-btn")
+  .addEventListener("click", deleteCommentFormHandler);
+
 document.querySelector('.comment-form').addEventListener('submit', handleComment);
