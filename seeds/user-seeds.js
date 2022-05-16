2363f130edbb072c4a7cae1c31706f3b7d99dcc3
Colt <98503459+coltschultz@users.noreply.github.com>
2022-05-15 20:15:40 -0500

    remove email from users

--- seeds/user-seeds.js
+++ seeds/user-seeds.js
@@ -4,17 +4,14 @@ const { User, Post } = require('../models');
 const userdata = [
   {
     username: 'larry',
-    email: 'larry@gmail.com',
     password: 'Changeme2%'
   },
   {
     username: 'mo',
-    email: 'mo@gmail.com',
     password: 'Changeme2%'
   },
   {
     username: 'curly',
-    email: 'curly@gmail.com',
     password: 'Changeme2%'
   }
   
